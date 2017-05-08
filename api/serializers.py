df14734bac2a2d52aa954000a5567cf1b2e9e029
Amos Omondi <amos.omondi@andela.com>
2017-05-08 16:22:02 +0300

    Finish writing serializers for models

--- api/serializers.py
+++ api/serializers.py
@@ -10,9 +10,11 @@ class NoteSerializer(ModelSerializer):
 
 
 class UserSerializer(ModelSerializer):
+    notes = NoteSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password', 'notes')
         # make sure password is not returned on read
         extra_kwargs = {
             'password': {'write_only': True}
