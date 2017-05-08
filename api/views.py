900b267b5ca6bef9d80f8f6b3b2b4f7c6dc56da5
Amos Omondi <amos.omondi@andela.com>
2017-05-08 17:38:56 +0300

    Add user permissions

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 from rest_framework.generics import ListCreateAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 
 from api.models import Note
 from api.serializers import UserSerializer, NoteSerializer
+from api.permissions import IsOwner
 
 
 class RegisterView(CreateAPIView):
@@ -21,11 +23,16 @@ class RegisterView(CreateAPIView):
 class NoteCreateView(ListCreateAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    permission_classes = (IsOwner,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    # def get_queryset(self):
+    #     return get_object_or_404(Note, owner=self.request.user)
 
 
 class NoteDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    permission_classes = (IsOwner,)
